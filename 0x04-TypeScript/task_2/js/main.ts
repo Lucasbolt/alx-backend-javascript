c34ae161d955c1fad9ed9261dc325b5d2a9c85e4
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-07-08 00:11:01 +0100

    tasks

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -27,16 +27,30 @@ export class Director implements DirectorInterface {
 export class Teacher implements TeacherInterface {
   workFromHome = () => "Cannot work from home";
   getCoffeeBreak = () => "Cannot have a break";
-  workTeacherTasks = () => "Gettinf to work";
+  workTeacherTasks = () => "Getting to work";
 }
 
-export function createEmployee(salary: number | string) {
+export function createEmployee(salary: number | string): (Director | Teacher) {
   if (typeof(salary) === 'number' && salary < 500)
-    return "Teacher";
+    return new Teacher();
   else (typeof(salary) === 'number' && salary >= 500)
-    return "Director";
+    return new Director();
 }
 
+export function isDirector(employee: (Director | Teacher)) {
+  return employee instanceof(Director);
+}
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
+export function executeWork(employee: (Director | Teacher)) {
+  if (employee instanceof(Director))
+    console.log((employee as Director).workDirectorTasks());
+  else
+     console.log((employee as Teacher).workTeacherTasks());
+}
+export type Subjects = ( "Math" | "History");
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math")
+    return "Teaching Math";
+  return "Teaching History";
+}
